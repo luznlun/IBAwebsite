ce25f14cf3fd8024e795eda7472b2609a2bc226d
Chris <chriscui828@yahoo.com>
2015-01-07 19:13:30 -0800

    content swap javascript done, waitting to get other pages done

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,6 +1,41 @@
 // JavaScript Document
+document.getElementById('nav01').innerHTML =
+"<div id='header'>"+
+"<span id='sj'> <a href='index.html'>SJSU International Business Association</a>"+
+"<a href='https://www.facebook.com/SJSUIBA' ><img src='img/facebook.png'/></a>"+
+"<a href='https://twitter.com/sjsuiba'><img src='img/twitter.png'/></a>"+
+"<a href='https://www.linkedin.com/groups/International-Business-Association-SJSU-Official-3526730/about'><img src='img/linkedin.png'/></a>"+
 
-function showContent(){
-	
-	
-}
+"</span>"+
+"</div>"+
+"<div id='nav'>"+
+"    <div id='nav_wrapper'>"+
+ "       <ul>"+
+  "          <li><a href='index.html'>Home</a>"+
+   "         </li>"+
+    "        <li> <a href='membership.html'>Membership</a>"+
+     "       </li>"+
+      "      <li> <a href='https://docs.google.com/forms/d/1WfEzQkuLMNpHa7QswbMSuUOB8jLXiDHo2QLxzsgiWvA/edit?usp=sharing'>Register</a>"+
+        "        <ul>"+
+              "      <li><a href='https://docs.google.com/forms/d/1WfEzQkuLMNpHa7QswbMSuUOB8jLXiDHo2QLxzsgiWvA/edit?usp=sharing'>IBA Spring 2014 Registration Form</a>"+
+             "   </ul>"+
+           " </li>"+
+           " <li> <a href='aboutus.html'>About Us</a>"+
+            "	<ul>"+
+"                    <li><a href='#'>Mission Statement</a>"+
+"                    </li>"+
+"                    <li><a href='#'>Executive Leadership Board Fall 2014</a>"+
+"                    </li>"+
+"                    <li><a href='#'>Executive Leadership Board Fall 2013</a>"+
+ "                   </li>"+
+  "                  <li><a href='#'>Founders & History</a>"+
+   "                 </li>"+
+    "            </ul>"+
+     "       </li>"+
+      "      <li><a href='contactus.html'>Contact Us</a>"+
+       "     </li>"+
+        "    <li><a href='#'>Jobs/Internships</a>"+
+         "   </li>"+
+"        </ul>"+
+ "   </div>"+
+"</div>";
